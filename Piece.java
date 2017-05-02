ea414502a1ef290af3328b96882932e64173dd45
SunWong <pt.s2.2017.sun.wong@gmail.com>
2017-05-02 11:46:10 +0200

    Update Piece.java

--- Piece.java
+++ Piece.java
@@ -1,97 +1,158 @@
 package SunWongParis13.Piece;
 
-public abstract class Piece { //abstraite ? 
-	
-	public static final String BLANC = "b";
-	public static final String NOIR = "n";
-	
-	public static final String ROI = "R";
-	public static final String DAME = "D";
-	public static final String TOUR = "T";
-	public static final String FOU = "F";
-	public static final String CAVALIER = "C";
-	public static final String PION = "P";
+public abstract class Piece {
 	
 	private String nom;
-	private boolean couleurPiece;
 	private int ligne;
 	private int colonne;
+	private boolean couleurPiece;
 	private boolean mouvement;
 	
-	public Piece(String p, boolean couleur, int l, int c)
-	{
-		this.setNom(p);
+	/**
+	 * 
+	 * Constructeur champ à champ
+	 * @param nomPiece : Nom de la piece
+	 * @param l : Ligne de la piece
+	 * @param c : Colonne de la piece
+	 * @param couleur : Couleur de la piece
+	 */
+	public Piece(String nomPiece, boolean couleur, int l, int c){
+		this.setNom(nomPiece);
 		this.setCouleurPiece(couleur);
 		this.setLigne(l);
 		this.setColonne(c);
-		this.setMouvement(false);
+		this.mouvement=false;
 	}
 	
-	//on regarde si le mouvement est correct
-	public boolean mouvementValide(int l, int c){
-		return (l>=0 && l<8 && c>=0 && c<8);
+	/**
+	 * 
+	 * Constructeur par copie
+	 * @param p : Piece à copier
+	 */
+	public Piece(Piece p){
+		this.setNom(new String(p.getNom()));
+		this.setCouleurPiece(p.getCouleurPiece());
+		this.setLigne(p.getLigne());
+		this.setColonne(p.getColonne());
+		this.mouvement=p.getMouvement();
 	}
 	
-	public void mouvementPiece(int l, int c){
-		if(mouvementValide(l,c) && mouvementIdentique(l,c)){
-			this.setLigne(l);
-			this.setColonne(c);
-			this.setMouvement(true);
-		}
-	}
-	
-	public boolean mouvementIdentique(int l, int c){
-		return this.getLigne()==l && this.getColonne()==c;
-	}
-	
-	//protected abstract void mouvement(int l, int c);
-	
-	//retourne le type de la piece
+	/**
+	 * 
+	 * @return Nom de la piece
+	 */
 	public String getNom() {
 		return this.nom;
 	}
 	
-	public void setNom(String piece) {
-		this.nom = piece;
-	}
-	
-	public boolean getCouleurPiece() {
-		return this.couleurPiece;
-	}
-
-	public void setCouleurPiece(boolean b) {
-		this.couleurPiece = b;
-	}
-	
+	/**
+	 * 
+	 * @return Ligne de la piece
+	 */
 	public int getLigne(){
 		return this.ligne;
 	}
-
-	public void setLigne(int ligne){
-		this.ligne = ligne;
-	}
-
+	
+	/**
+	 * 
+	 * @return Colonne de la piece
+	 */
 	public int getColonne(){
 		return this.colonne;
 	}
-
-	public void setColonne(int colonne){
-		this.colonne = colonne;
+	
+	/**
+	 * 
+	 * @return Couleur de la piece (blanc ou noir)
+	 */
+	public boolean getCouleurPiece() {
+		return this.couleurPiece;
 	}
 	
-	//on regarde si la piece s'est deplace
+	/**
+	 * 
+	 * @return Si la piece s'est déplacée
+	 */
 	public boolean getMouvement(){
 		return this.mouvement;
 	}
 	
-	//on utilise la fonction apres un mouvement
-	public void setMouvement(boolean b){
-		this.mouvement = b;
+	/**
+	 * 
+	 * @param nomPiece : Nom de la piece
+	 */
+	public void setNom(String nomPiece) {
+		this.nom = nomPiece;
 	}
 	
+	/**
+	 * 
+	 * @param ligne : Ligne de la piece
+	 */
+	public void setLigne(int ligne){
+		this.ligne = ligne;
+	}
+
+	/**
+	 * 
+	 * @param colonne : Colonne de la piece
+	 */
+	public void setColonne(int colonne){
+		this.colonne = colonne;
+	}
+	
+	/**
+	 * 
+	 * @param couleur : Couleur de la piece
+	 */
+	public void setCouleurPiece(boolean couleur) {
+		this.couleurPiece = couleur;
+	}
+	
+	/**
+	 * La piece a bougé
+	 */
+	public void setMouvement(){
+		this.mouvement = true;
+	}
+	
+	/**
+	 * 
+	 * @param l : Ligne de la piece
+	 * @param c : Colonne de la piece
+	 * @return Verifie la piece concerne peut faire ce deplacement
+	 */
+	public abstract boolean deplacementValide(int l, int c);
+	
+	/**
+	 * 
+	 * @param l : Ligne de la piece
+	 * @param c : Colonne de la piece
+	 * @return Verifie si il y a eu un mouvement
+	 */
+	public boolean mouvementIdentique(int l, int c){
+		return this.getLigne()==l && this.getColonne()==c;
+	}
+	
+	/**
+	 * Verifie si la piece peut se deplacer, si elle bouge, si elle est dans la piece
+	 * @param l : Ligne de la piece
+	 * @param c : Colonne de la piece
+	 */
+	public void mouvementPiece(int l, int c){
+		if(deplacementValide(l,c) && !mouvementIdentique(l,c)){
+			this.setLigne(l);
+			this.setColonne(c);
+			this.setMouvement();
+		}
+	}
+
+	/**
+	 * On affiche le nom de la piece + la couleur de la piece
+	 */
 	public String toString(){
 		if(this.couleurPiece == true)
-			return this.getNom() + BLANC;
-		return this.getNom() + NOIR;
+			return this.getNom() + Piece_couleur_constantes.BLANC;
+		return this.getNom() + Piece_couleur_constantes.NOIR;
 	}
 }
