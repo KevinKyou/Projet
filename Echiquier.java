e5d82bbcc0909c04dda586d9206fb5c94e794fc6
SunWong <pt.s2.2017.sun.wong@gmail.com>
2017-06-02 21:08:59 +0200

    Update Echiquier.java

--- Echiquier.java
+++ Echiquier.java
@@ -43,14 +43,9 @@ public class Echiquier {
 				this.setGrille(new Case(new PieceRoi(false, 0, i), 0, i));
 				this.setGrille(new Case(new PieceRoi(true, 7, i), 7, i));
 			}
-			//this.grille[1][i] = new Case(new PiecePion(false, 1, i), 1, i);
-			//this.grille[6][i] = new Case(new PiecePion(true, 6, i), 6, i);
+			this.grille[1][i] = new Case(new PiecePion(false, 1, i), 1, i);
+			this.grille[6][i] = new Case(new PiecePion(true, 6, i), 6, i);
 		}
-			//this.setGrille(new Case(new PieceCavalier(false, 5, 3), 5, 3));
-		
-		this.setGrille(new Case(new PieceTour(false, 2, 4), 2, 4));
-		this.setGrille(new Case(new PieceTour(true, 5, 1), 5, 1));
-		
 	}
 	
 	public Case getGrille(int l, int c) {
@@ -61,50 +56,12 @@ public class Echiquier {
 		this.grille[piece.getLigne()][piece.getColonne()] = piece;
 	}
 	
-	public String toString(){
-		StringBuilder s = new StringBuilder("       1        2        3        4        5        6        7        8   ").append(System.lineSeparator());
-		s.append("  ┌────────┬────────┬────────┬────────┬────────┬────────┬────────┬────────┐").append(System.lineSeparator());
-		int c = 65;
-		
-		
-		for(int i=0; i<8; i++){
-			s.append("  │");
-			for(int j=0; j<8; j++){
-					if(this.grille[i][j].getCaseAutorise())
-						s.append(" ┌────┐ │"); //c1
-					else
-						s.append("        │");						
-			}
-			s.append(System.lineSeparator());
-			s.append((char)(c+i) + " │");	 //c2
-			for(int j=0; j<8; j++){
-				if(this.grille[i][j].getCaseRemplie())
-					if(this.grille[i][j].getCaseAutorise())
-						s.append(" │ " + this.grille[i][j]).append(" │ │");//c2
-					else
-						s.append("   " + this.grille[i][j]).append("   │");//c2
-				else
-					if(this.grille[i][j].getCaseAutorise())
-						s.append(" │  ").append("  │ │");//c2
-					else
-						s.append("    ").append("    │");//c2
-			}
-			s.append(System.lineSeparator());
-			s.append("  │");
-			for(int j=0; j<8; j++){
-					if(this.grille[i][j].getCaseAutorise())
-						s.append(" └────┘ │"); //c1
-					else
-						s.append("        │");						
-			}
-			s.append(System.lineSeparator());
-			if(i<7)
-				s.append("  ├────────┼────────┼────────┼────────┼────────┼────────┼────────┼────────┤").append(System.lineSeparator());
-			else
-				s.append("  └────────┴────────┴────────┴────────┴────────┴────────┴────────┴────────┘").append(System.lineSeparator());
-		}
-		return s.toString();
-	}
+	//public String toString()
+	
+	
+	
+	
+	
 }
 
 
