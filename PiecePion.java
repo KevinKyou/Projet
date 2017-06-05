cfda3449400afc9ee6122a1d1c2824b32d2dc4cb
SunWong <pt.s2.2017.sun.wong@gmail.com>
2017-06-05 17:48:45 +0200

    Update PiecePion.java

--- PiecePion.java
+++ PiecePion.java
@@ -2,41 +2,51 @@ package SunWongParis13.Piece;
 
 public class PiecePion extends Piece{
 
+	/**
+	 * Constructeur du Pion
+	 * @param couleur : Couleur du Pion
+	 * @param ligne : Ligne de la grille
+	 * @param colonne : Colonne de la grille
+	 */	
 	public PiecePion(boolean couleur, int ligne, int colonne){
-		super(Piece.PION, couleur, ligne, colonne);
+		super(Piece_nom_constantes.PION, couleur, ligne, colonne);
 	}
 
-	public boolean mouvementPion(int l, int c){
-		if(mouvementValide(l,c) && !(mouvementIdentique(l,c)))
-			return true;
-		return false;
-	}
-
-	public boolean deplacementValideBlanc(int l, int c){
-		if(this.getMouvement() == false)
-			return (this.getLigne()-2==l || this.getLigne()-1==l) && this.getColonne()==c;
+	/**
+	 * Deplacement en avant pour les blancs et en arrière pour les noirs
+	 * @param l : Ligne de la piece
+	 * @param c : Colonne de la piece
+	 * @return Verifie si le pion peut avancer d'une ou deux cases en meme temps
+	 */
+	public boolean deplacementValide(int l, int c){
+		if(this.getMouvement() == 0)
+			if(this.getCouleurPiece()==true)
+				return ((this.getLigne()-2==l || this.getLigne()-1==l) && this.getColonne()==c);
+			else
+				return (this.getLigne()+2==l || this.getLigne()+1==l) && this.getColonne()==c;
 		else
-			return this.getLigne()-1==l && this.getColonne()==c;
-	}
-	
-	public boolean deplacementValideVersusBlanc(int l, int c){
-		if(this.getMouvement() == false)
-			return (this.getLigne()-2==l || this.getLigne()-1==l) && (this.getColonne()==c || this.getColonne()-1==c || this.getColonne()+1==c);
-		else
-			return this.getLigne()-1==l && (this.getColonne()==c || this.getColonne()-1==c || this.getColonne()+1==c) ;
-	}
-	
-	public boolean deplacementValideNoir(int l, int c){
-		if(this.getMouvement() == false)
-			return (this.getLigne()+2==l || this.getLigne()+1==l) && this.getColonne()==c;
-		else
-			return this.getLigne()+1==l && this.getColonne()==c;
+			if(this.getCouleurPiece()==true)
+				return this.getLigne()-1==l && this.getColonne()==c;
+			else
+				return this.getLigne()+1==l && this.getColonne()==c;
 	}
 	
-	public boolean deplacementValideVersusNoir(int l, int c){
-		if(this.getMouvement() == false)
-			return (this.getLigne()+2==l || this.getLigne()+1==l) && (this.getColonne()==c || this.getColonne()-1==c || this.getColonne()+1==c);
-		else
+	/**
+	 * Deplacement en diagonale pour les blancs/noirs
+	 * @param l : Ligne de la piece
+	 * @param c : Colonne de la piece
+	 * @return Verifie si le pion peut avancer d'une ou deux cases en meme temps
+	 */
+	public boolean deplacementValideVersus(int l, int c){
+		if(this.getCouleurPiece()==true){
+			if(this.getMouvement() == 0)
+				return this.getLigne()-1==l && (this.getColonne()-1==c || this.getColonne()+1==c);
+			return this.getLigne()-1==l && (this.getColonne()==c || this.getColonne()-1==c || this.getColonne()+1==c);
+		}
+		else{
+			if(this.getMouvement() == 0)
+				return this.getLigne()+1==l && (this.getColonne()-1==c || this.getColonne()+1==c);
 			return this.getLigne()+1==l && (this.getColonne()==c || this.getColonne()-1==c || this.getColonne()+1==c) ;
+		}
 	}
 }
