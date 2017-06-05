87f057dcf4cd4b6bf00c9aa5f34114497436880d
SunWong <pt.s2.2017.sun.wong@gmail.com>
2017-06-05 17:57:28 +0200

    Create JeuEchec.java

---
+++ JeuEchec.java
@@ -0,0 +1,10 @@
+package SunWongParis13.Jeu;
+
+import SunWongParis13.Echiquier.EchiquierJeu;
+
+public class JeuEchec {
+	public static void main(String [] args){
+		EchiquierJeu a = new EchiquierJeu();
+		a.jouerJeu();
+	}
+}
