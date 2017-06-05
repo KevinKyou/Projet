602bd733f920849281070a6673fad6c626778ce5
SunWong <pt.s2.2017.sun.wong@gmail.com>
2017-06-05 17:49:16 +0200

    Update PieceTour.java

--- PieceTour.java
+++ PieceTour.java
@@ -2,19 +2,24 @@ package SunWongParis13.Piece;
 
 public class PieceTour extends Piece{
 	
+	/**
+	 * Constructeur de la Tour
+	 * @param couleur : Couleur de la Tour
+	 * @param ligne : Ligne de la grille
+	 * @param c : Colonne de la grille
+	 */
 	public PieceTour(boolean couleur, int ligne, int colonne){
-		super(Piece.TOUR, couleur, ligne, colonne);
+		super(Piece_nom_constantes.TOUR, couleur, ligne, colonne);
 	}
-	
-	public boolean mouvementTour(int l, int c){
-		if(deplacementValide(l,c) && mouvementValide(l,c) && !(mouvementIdentique(l,c)))
-			return true;
-		return false;
-	}
-	
+
+	/**
+	 * Deplacement Haut/Bas -- Gauche/Droit
+	 * @param l : Ligne de la piece
+	 * @param c : Colonne de la piece
+	 * @return Verifie si la tour se deplace à l'horizontale et à la verticale
+	 */
 	public boolean deplacementValide(int l, int c){
 		return (this.getLigne()>=0 && this.getLigne()<=7 && this.getColonne()==c) ||
 				(this.getLigne()==l && this.getColonne()>=0 && this.getColonne()<=7);
 	}
-
 }
