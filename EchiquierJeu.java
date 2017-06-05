036251c0470140caf3661491f49c8f6c360c2fbb
SunWong <pt.s2.2017.sun.wong@gmail.com>
2017-06-05 18:02:35 +0200

    Update EchiquierJeu.java

--- EchiquierJeu.java
+++ EchiquierJeu.java
@@ -1,28 +1,27 @@
 package SunWongParis13.Echiquier;
 
 public class EchiquierJeu{
-	
 	private Echiquier jeu;
-  
-  public EchiquierJeu() {
+  	
+	public EchiquierJeu() {
 		this.jeu = new Echiquier();
 		this.jeu.initialiseEchiquier();
 		tour = true;
 	}
   
-  public boolean saisieValide(int g_l, int g_c){
+  	public boolean saisieValide(int g_l, int g_c){
 		return g_l>=0 && g_l<=7 && g_c>=0 && g_c<=7;
 	}
   
-  /*
-  *
-  *
-  * FONCTION
-  *
-  */
+	/*
+	 *
+	 *
+	 * FONCTION
+	 *
+	 */
   
   
-  
-  public String toString(){
+	public String toString(){
 		return jeu.toString();
 	}
+}
