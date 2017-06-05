8f693927490a9e6331ed13a46f7bf4b4bf59bea1
SunWong <pt.s2.2017.sun.wong@gmail.com>
2017-06-05 17:49:00 +0200

    Update PieceRoi.java

--- PieceRoi.java
+++ PieceRoi.java
@@ -1,21 +1,28 @@
 package SunWongParis13.Piece;
 
 public class PieceRoi extends Piece{
-
-	public PieceRoi(boolean couleur, int ligne, int colonne){
-		super(Piece.ROI, couleur, ligne, colonne);
-	}
-
-	public boolean mouvementRoi(int l, int c){
-		if(deplacementValide(l, c) && mouvementValide(l,c) && !(mouvementIdentique(l,c))) //une case autour de lui, dep case 1 a 6, !faux si les piece n'ont pas bouge
-			return true;
-		return false;
-	}
 	
+	/**
+	 * Constructeur du Roi
+	 * @param couleur : Couleur du Fou
+	 * @param ligne : Ligne de la grille
+	 * @param colonne : Colonne de la grille
+	 */
+	public PieceRoi(boolean couleur, int ligne, int colonne){
+		super(Piece_nom_constantes.ROI, couleur, ligne, colonne);
+	}
+
+	/**
+	 * Deplacement autour de lui
+	 * @param l : Ligne de la piece
+	 * @param c : Colonne de la piece
+	 * @return Verifie si le roi se deplace a une ligne et/ou colonne de lui
+	 */
 	public boolean deplacementValide(int l, int c){
-		if(this.getMouvement() == false)
-			return (this.getLigne()-1==l || this.getLigne()+1==l) && (this.getColonne()-1==c || this.getColonne()==c || this.getColonne()+1==c);
+		if(this.getMouvement() == 0)
+			return (this.getLigne()==l && (this.getColonne()-2==c || this.getColonne()+2==c)) ||
+					((this.getLigne()-1==l || this.getLigne()+1==l || this.getLigne()==l) && (this.getColonne()==c || this.getColonne()-1==c || this.getColonne()+1==c));
 		else
-			return (this.getLigne()-1==l || this.getLigne()+1==l) && this.getColonne()==c;
+			return (this.getLigne()-1==l || this.getLigne()+1==l || this.getLigne()==l) && (this.getColonne()==c || this.getColonne()-1==c || this.getColonne()+1==c);
 	}
 }
