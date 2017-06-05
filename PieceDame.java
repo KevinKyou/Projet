6252dba0258c34d1a30df5c43375f8597939d77d
SunWong <pt.s2.2017.sun.wong@gmail.com>
2017-06-05 17:48:15 +0200

    Update PieceDame.java

--- PieceDame.java
+++ PieceDame.java
@@ -2,39 +2,33 @@ package SunWongParis13.Piece;
 
 public class PieceDame extends Piece{
 	
-	public PieceDame(boolean couleur, int ligne, int colonne){
-		super(Piece.DAME, couleur, ligne, colonne);
-	}
-
-	public boolean mouvementDame(int l, int c){
-		if(deplacementValide(l,c) && mouvementValide(l,c) && !(mouvementIdentique(l,c)))
-			return true;
-		return false;
-	}
+	/**
+	 * Constructeur de la Dame
+	 * @param couleur : Couleur de la Dame
+	 * @param ligne : Ligne de la grille
+	 * @param c : Colonne de la grille
+	 */
+	public PieceDame(boolean couleur, int l, int c){
+		super(Piece_nom_constantes.DAME, couleur, l, c);
+	}	
 	
-	
+	/**
+	 * Deplacement Diagonale -- Haut/Bas -- Gauche/Droite
+	 * @param l : Ligne de la piece
+	 * @param c : Colonne de la piece
+	 * @return Verifie si la dame se deplace en diagonale, à l'horizontal ou à la verticale
+	 */
 	public boolean deplacementValide(int l, int c){
-		if(this.getLigne()>l && this.getColonne()>c){ //hg
+		if(this.getLigne()>l && this.getColonne()>c)
 			return (this.getLigne() - l) == (this.getColonne() - c);
-		}
-		else if(this.getLigne()>l && this.getColonne()<c){ //hd
+		else if(this.getLigne()>l && this.getColonne()<c)
 			return (this.getLigne() - l) == (c - this.getColonne());
-		}
-		else if(this.getLigne()<l && this.getColonne()>c){ //bg
+		else if(this.getLigne()<l && this.getColonne()>c)
 			return (l - this.getLigne()) == (this.getColonne() - c);
-		}
-		else if(this.getLigne()<l && this.getColonne()<c){ //bd
+		else if(this.getLigne()<l && this.getColonne()<c)
 			return (l - this.getLigne()) == (c - this.getColonne());
-		}
+		
 		return (this.getLigne()>=0 && this.getLigne()<=7 && this.getColonne()==c) ||
 				(this.getLigne()==l && this.getColonne()>=0 && this.getColonne()<=7);
-	}
-	
-	public static void main(String [] args){
-		PieceDame f = new PieceDame(true, 0, 0);
-		if(f.deplacementValide(7,7))
-			System.out.println("bingo");
-		else
-			System.out.println("bango");
 	}
 }
