9c6c38d27e362ebf972d3f4093dceda0b4d6e0ba
SunWong <pt.s2.2017.sun.wong@gmail.com>
2017-06-05 17:47:59 +0200

    Update PieceCavalier.java

--- PieceCavalier.java
+++ PieceCavalier.java
@@ -1,18 +1,24 @@
 package SunWongParis13.Piece;
 
 public class PieceCavalier extends Piece{
-	 
-	public PieceCavalier(boolean couleur, int ligne, int colonne){
-		super(Piece.CAVALIER, couleur, ligne, colonne);
-	}
 	
-	public boolean mouvementCavalier(int l, int c){
-		if(deplacementValide(l,c) && mouvementValide(l,c) && !(mouvementIdentique(l,c)))
-			return true;
-		return false;
+	/**
+	 * Constructeur du Cavalier
+	 * @param couleur : Couleur du Cavalier
+	 * @param ligne : Ligne de la grille
+	 * @param colonne : Colonne de la grille
+	 */
+	public PieceCavalier(boolean couleur, int l, int c){
+		super(Piece_nom_constantes.CAVALIER, couleur, l, c);
 	}
-	
-	public boolean deplacementValide(int l, int c){
+
+	/**
+	 * Deplacement en L
+	 * @param l : Ligne de la piece
+	 * @param c : Colonne de la piece
+	 * @return Verifie si le cavalier se deplace en L
+	 */
+	public boolean deplacementValide(int l, int c) {
 		return (this.getLigne()-2==l && this.getColonne()+1==c) || 
 				(this.getLigne()-1==l && this.getColonne()+2==c) || 
 				(this.getLigne()+1==l && this.getColonne()+2==c) ||
@@ -24,4 +30,3 @@ public class PieceCavalier extends Piece{
 				;
 	}
 }
-
